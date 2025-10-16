097dbd877a82eca940e01758216051ddd5104938
yohannricq <yohann.ricq@gmail.com>
2025-10-16 12:00:50 +0200

    connexion avec DataSource

--- src/main/java/com/mycompany/myapp/TestConnection.java
+++ src/main/java/com/mycompany/myapp/TestConnection.java
@@ -6,19 +6,28 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.postgresql.ds.PGSimpleDataSource;
+
 public class TestConnection {
 
     public static void main(String... args){
         Connection conn = null;
         try {
             
+            PGSimpleDataSource dataSource = new PGSimpleDataSource();
+
+            String url = "jdbc:postgresql://localhost:5432/tennis";
+            dataSource.setURL(url);
+            dataSource.setUser("postgres");
+            dataSource.setPassword("p@ssw0rd");
             //MySQL driver MySQL Connector
             //conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","COURSDB","COURSDB");
             //Oracle Driver officiel OJDBC Thin
             //conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:tennis","COURSDB","COURSDB");
             //Postgres Driver officiel
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tennis","postgres","p@ssw0rd");
+            //conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tennis","postgres","p@ssw0rd");
 
+            conn = dataSource.getConnection();
             conn.setAutoCommit(false);
 
             PreparedStatement preparedStatement=conn.prepareStatement("INSERT INTO joueur(nom, prenom, sexe) VALUES(?, ?, ?)");
